dedbb19d8e7e0333fdb061b192ea27f763a4d804
jiangergo <jiangergo@163.com>
2016-09-08 11:23:42 +0800

    高大神初始化了项目

---
+++ LoveBee/LoveBee/Classes/View/Main/LBTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  LBTabBarController.h
+//  LoveBee
+//
+//  Created by vane on 16/9/8.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBTabBarController : UITabBarController
+
+@end
