0881126073a95ed8b5216b242674a54a19f1c48d
YINMonkey <vane@GY.local>
2016-09-09 20:25:10 +0800

    Revert "闪送界面"
    This reverts commit f4c40a63075ea58f9de937554cb83d7af8cd38f1.
    
    # Conflicts:
    #
    LoveBee/LoveBee.xcworkspace/xcuserdata/vane.xcuserdatad/UserInterfaceSta
    te.xcuserstate

--- LoveBee/LoveBee/Classes/ViewModel/LBSuperLeftTableViewModel.m
+++ LoveBee/LoveBee/Classes/ViewModel/LBSuperLeftTableViewModel.m
@@ -22,7 +22,7 @@
 -(void)setValue:(id)value forUndefinedKey:(NSString *)key{
     
 }
-+(void)categoriesWithSuccess:(void(^)(NSArray<LBSuperLeftTableViewModel *> *array))successBlock error:(void(^)())errorBlock{
++(void)productWithLeftTableView:(BOOL)isTrue success:(void(^)(NSArray<LBSuperLeftTableViewModel *> *array))successBlock error:(void(^)())errorBlock{
     
     NSMutableArray *mArray = [NSMutableArray array];
     
@@ -35,12 +35,21 @@
         
         NSDictionary *data = responseObject[@"data"];
         
-        NSArray *array = data[@"categories"];
+        NSArray *array = [NSArray array];
+        
+        if (isTrue) {
+            
+        array = data[@"categories"];
+            
+        }else{
+         
+            array = data[@"products"];
+        }
+
         
         [array enumerateObjectsUsingBlock:^(NSDictionary*  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
             
             LBSuperLeftTableViewModel *model = [LBSuperLeftTableViewModel modelWithDict:obj];
-            
             [mArray addObject:model];
             
                 
@@ -49,44 +58,6 @@
         
          if (successBlock) {
              successBlock(mArray.copy);
-        }
-    } failure:^(NSError *error) {
-        if (error) {
-            errorBlock(error);
-        }
-    }];
-    
-}
-+(void)productWithID:(NSString *)ID success:(void(^)(NSArray<LBSuperLeftTableViewModel *> *array))successBlock error:(void(^)())errorBlock{
-    
-    NSMutableArray *mArray = [NSMutableArray array];
-    
-    AFHTTPTool *manager = [AFHTTPTool sharedManager];
-    
-    
-    NSDictionary *parm = @{@"call":@"5"};
-    
-    [manager postWithUrl:@"http://iosapi.itcast.cn/loveBeen/supermarket.json.php" params:parm success:^(NSDictionary* responseObject) {
-        
-        NSDictionary *data = responseObject[@"data"];
-        
-        NSDictionary *productData = data[@"products"];
-        
-        NSArray *array = productData[ID];
-        NSLog(@"ID:%@",ID);
-        
-        [array enumerateObjectsUsingBlock:^(NSDictionary*  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            
-            LBSuperLeftTableViewModel *model = [LBSuperLeftTableViewModel modelWithDict:obj];
-            
-            [mArray addObject:model];
-            
-            
-        }];
-        
-        
-        if (successBlock) {
-            successBlock(mArray.copy);
         }
     } failure:^(NSError *error) {
         if (error) {
